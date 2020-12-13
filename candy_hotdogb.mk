8715692a13e7ebad4fec6754e25eb5ec6f419b33
NoSpamDan <nospamdan1@gmail.com>
2020-12-12 22:13:59 -0500

    hotdogb: Update fingerprint for Dec 2020 security patches

--- candy_hotdogb.mk
+++ candy_hotdogb.mk
@@ -35,4 +35,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7T_EEA-user 10 QKQ1.190716.003 1910122101 release-keys"
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys
