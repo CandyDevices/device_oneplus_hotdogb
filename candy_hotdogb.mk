79e52e8b965c0d7bb26f25732bae69eff7d23960
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-04-08 14:39:58 +0530

    hotdogb: fix product props
    * set default as Global model, correct props will be set later by libinit
    * don't set build desc
    * fingerprint is being set by libinit as well

--- candy_hotdogb.mk
+++ candy_hotdogb.mk
@@ -18,7 +18,7 @@ $(call inherit-product, vendor/candy/config/common_full_phone.mk)
 PRODUCT_NAME := candy_hotdogb
 PRODUCT_DEVICE := hotdogb
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := OnePlus 7T
+PRODUCT_MODEL := HD1905
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_AAPT_CONFIG := normal
@@ -31,8 +31,8 @@ TARGET_SCREEN_WIDTH := 1080
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7T \
-    PRODUCT_NAME=OnePlus7T \
-    PRIVATE_BUILD_DESC="hotdogb-user 10 QKQ1.190716.003 2011052234 release-keys"
+    PRODUCT_NAME=OnePlus7T
+
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 BUILD_FINGERPRINT := google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys
