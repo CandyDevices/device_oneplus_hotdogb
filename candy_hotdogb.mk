22cdc9ea8d72a69ac1a345942ff23dd67b14cf94
NoSpamDan <nospamdan1@gmail.com>
2021-04-18 20:28:26 -0400

    hotdogb: Update fingerprint to April Redfin

--- candy_hotdogb.mk
+++ candy_hotdogb.mk
@@ -28,11 +28,15 @@ PRODUCT_CHARACTERISTICS := nosdcard
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7T \
-    PRODUCT_NAME=OnePlus7T
+    PRODUCT_NAME=OnePlus7T \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-BUILD_FINGERPRINT := google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint= google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
