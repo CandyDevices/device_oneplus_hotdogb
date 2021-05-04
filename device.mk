c33ce5094c9604731cce6680e490ac713cb36f32
NoSpamDan <nospamdan1@gmail.com>
2021-05-03 22:58:36 -0400

    hotdogb: Add Notch Bar Killer overlay
    this overlay gets rid of the black bar in most apps/games and lets the screen fill past the notch.
    
    xlxfoxxlx: reworked for inline building and crosshatch
    anirudhgupta109: modified for hotdogb dimensions
    
    Change-Id: Ic008ce5d569127d230dc44451929477e063c4471

--- device.mk
+++ device.mk
@@ -32,6 +32,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
 
+# Notch hide
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay \
+    NotchBarKiller
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
